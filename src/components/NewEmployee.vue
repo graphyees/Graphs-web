499d307794eb2c991d7a205285d82d93a7a1fe4e
Nerea <nereaparicio@gmail.com>
2019-06-02 22:03:35 +0200

    create a new employee works

--- src/components/NewEmployee.vue
+++ src/components/NewEmployee.vue
@@ -1,16 +1,70 @@
 <template>
   <div class="hello">
-   <h1>New Employee </h1>
+    <h1>New Employee </h1>
+    <div>
+      <form>
+        <div class="form-group">
+          <label for="name">Employee Name</label>
+          <input v-model="name" class="input form-control" type="text" id="name" aria-describedby="emailHelp" placeholder="Enter employee name">
+        </div>
+        <div class="form-group">
+          <label for="salary">Salary</label>
+          <input v-model="salary" type="number" class="form-control" id="salary" aria-describedby="emailHelp" placeholder="Enter the salary in number">
+        </div>  
+        <div class="form-group">
+          <label for="department">Department</label>
+          <select v-model="department"  type="text" class="form-control" id="department" placeholder="select a department">
+            <option>admin</option>
+            <option>support</option>
+            <option>contracts</option>
+            <option>proyects</option>
+            <option>rrhh</option>
+          </select>
+        </div>
+          <button type="button" value="Submit" class="btn btn-success" @click = sendEmployeeData(name,salary,department)>Add</button>
+      </form>
+    </div>
   </div>
 </template>
 
 
 <script>
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios); 
+
 export default {
-  name: 'NewEmployee',
+  name: 'NewWmployee',
+  data () {
+    return {
+      name:'',
+      salary: null,
+      department: ''
+    }
+  },
+  mounted(){
+    this.sendEmployeeData
+  },
+  methods: {
+    sendEmployeeData: function (name, salary, department) {
+        axios.post('http://localhost:3001/my-employees/newemployee', {
+            name: name,
+            salary: salary,
+            department: department
+        }).then(response => {
+            if (event) event.preventDefault()
+            alert("Employee registered")
+        }).catch(error => {
+            console.log(error);
+        if (event) event.preventDefault()
+          alert("Employee not registered\n" + error)
+        });
+    } 
+  }
 }
 </script>
-
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
