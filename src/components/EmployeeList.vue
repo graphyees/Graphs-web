76c574ffe8afb5927df3ba26d050c31bedaed712
Nerea <nereaparicio@gmail.com>
2019-06-02 10:49:32 +0200

    add new component, needs the routes

---
+++ src/components/EmployeeList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+   <h1>Employee list </h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'EmployeeList',
+}
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: flow-root;
+  margin: 5px 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
