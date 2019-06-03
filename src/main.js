259cdad47cf644c10e99a994c0b12ea07111e377
Nerea <nereaparicio@gmail.com>
2019-06-03 18:43:17 +0200

    working on a graph

--- src/main.js
+++ src/main.js
@@ -13,33 +13,6 @@ new Vue({
   router,
   store,
   render: h => h(App),
- 
 }).$mount("#app");
 
 Vue.use(axios, vueAxios);
-
-// new Vue({
-//   el: "#app",
-//   router,
-//   //store,
-//   //template: '<App/>',
-//   component: {
-//     App
-//   },
-//   //render: h => h(App),
-//   data() {
-//     return {
-//       info: null
-//     };
-//   },
-//   // mounted() {
-//   //   axios
-//   //     .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-//   //     .then(response => {
-//   //       console.log(response);
-//   //      })
-//   //      .catch(error => {
-//   //       console.log(error);
-//   //      });
-//   // }
-// });
