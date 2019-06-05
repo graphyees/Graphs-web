ca0043ac71f74c8fa80c6ecc73eebdaad21df98d
Nerea <nereaparicio@gmail.com>
2019-06-05 20:51:52 +0200

    return

--- src/components/Graphys.vue
+++ src/components/Graphys.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="hello">
    <h1>Graficas </h1>
+
+        <div class="form-group">
+          <label for="department">Department</label>
+          <select v-model="department" type="text" class="form-control" id="department" placeholder="select a department">
+            <option>admin</option>
+            <option>support</option>
+            <option>contracts</option>
+            <option>proyects</option>
+            <option>rrhh</option>
+          </select>
+        </div>
+      <div class="graphys">
+        <div class="card">
+
+        </div>
+      </div>
   </div>
 </template>
 
